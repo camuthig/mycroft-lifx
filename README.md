e89a31fcedf44c54be4ce2e12ba1599592dfcb76
Chris Muthig <camuthig@gmail.com>
2018-01-15 14:14:05 -0800

    Add readme and functionality to list lights

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
 # mycroft-lifx
 
 A Mycroft skill for controlling Lifx bulbs.
+
+## Features
+
+[ ] Set API key using Mycroft settings
+[ ] Set the devices room using Mycroft settings
+[ ] Use regex for better matching
+[x] Connect to Lifx
+[x] Turn lights on/off
+[ ] Dim/bright lights **(in progress)**
+[ ] Set the color of lights (color)
+[ ] Change the warmth of the lights (white)
+[x] List lights
+
+## Commands
+
+### Connecting to Lifx
+
+When first loaded, this skill will connect to Lifx to determine the names of
+all rooms and lights in your account. This is important so that it can listen
+for these names in your commands. If you ever decide to change the names of the
+lights or rooms, you should issue a command for the skill to refresh it's list
+
+`Reconnect to Lifx`
+
+### Turning lights on/off
+
+To turn on the lights, you can use a simple command like:
+
+`Turn on the lights`
+
+Or to turn them off,
+
+`Turn off the lights`
+
+This will turn on _all_ of the lights though, which isn't always helpful.
+Another option is to access devices by their name (label) or room (group). For
+example, if you have a group in your Lifx settings called "Livingroom", you
+could just say something like
+
+`Turn on the livingroom lights`
+
+to turn on only the lights in the livingroom. The skill supports both
+individual light names and group names.
+
+### Changing the brightness
+
+To Be Implemented
+
+Lights can be dimmed individually, by name, or as a group. To do so, just issue
+a command like
+
+`Dim the lights in the livingroom`
+
+This will dim the lights by 10% for each time the command is executed.
+
+### Listing lights
+
+So maybe you don't know the names of all the lights in the current room. Don't
+worry, this skill can help. By issuing a command like
+
+`What are the names of the lights in here?`
+
+or
+
+`What are the names of the lights in the livingroom?`
+
+you can figure it out. This command will have mycroft list each of the lights
+in the room, while pulsing the light, so you know which one it is.
--- __init__.py
+++ __init__.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
 # coding=utf-8
-from adapt.intent import IntentBuilder
 from os import path
 import time
 import timeit
+from collections import defaultdict
+from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import getLogger
-
-import pprint
 
 from pifx import PIFX
 
 LOGGER = getLogger(__name__)
 
 class LifxSkill(MycroftSkill):
+    """
+    A Mycroft skill for controlling Lifx devices via the HTTP API.
+    """
+
     def __init__(self):
         super(LifxSkill, self).__init__(name="LifxSkill")
-        # @TODO Get the key from the the settingsmeta.json instead
+        # TODO Get the key from the the settingsmeta.json instead
         self.lifx = PIFX(self._get_key())
 
         # These will be set by _collect_selectors
         self.labels = {}
         self.groups = {}
+        self.room_lights = {}
 
     def initialize(self):
         self._collect_selectors()
+
+    def stop(self):
+        pass
 
     def _get_key(self):
         key_file = open(path.join(path.abspath(path.dirname(__file__)), 'api_key'), 'r')
@@ -34,6 +41,7 @@ class LifxSkill(MycroftSkill):
         return key
 
     def _collect_selectors(self):
+        self.room_lights = defaultdict(list)
         lights = self.lifx.list_lights()
         for light in lights:
             label = light.get("label")
@@ -48,6 +56,7 @@ class LifxSkill(MycroftSkill):
                 self.register_vocabulary(normalized, "Group")
 
                 self.groups[normalized] = group
+                self.room_lights[normalized].append(label)
 
     def _normalize_selector(self, selector):
         return selector.lower().replace("'", "")
@@ -55,9 +64,9 @@ class LifxSkill(MycroftSkill):
     def _choose_selector(self, message):
         selector = "all"
         if "Group" in message.data:
-            selector = "group:" + self.groups.get(message.data["Group"].lower())
+            selector = "group:" + self.groups.get(self._normalize_selector(message.data["Group"]))
         elif "Label" in message.data:
-            selector = "label:" + self.labels.get(message.data["Label"].lower())
+            selector = "label:" + self.labels.get(self._normalize_selector(message.data["Label"]))
 
         return selector
 
@@ -82,5 +91,43 @@ class LifxSkill(MycroftSkill):
         self.lifx.set_state(self._choose_selector(message), "off")
         self.speak_dialog("turn.off")
 
+    @intent_handler(IntentBuilder("ListLightsIntent")
+        .require("LightsKeyword")
+        .require("InKeyword")
+        .one_of("Group", "RoomKeyword"))
+    def handle_list_lights_intent(self, message):
+        room = "here"
+        # TODO use settings to configure the default room for the device
+        lights = self.labels
+
+        if "Group" in message.data:
+            room = self.groups.get(message.data["Group"])
+            lights = self.room_lights.get(message.data["Group"])
+
+        self.speak("The lights in {} are".format(room))
+
+        for light in lights:
+            self.lifx.pulse_lights("white", selector="label:" + light, cycles=1.0)
+            self.speak(light)
+
+    @intent_handler(IntentBuilder("ChangeBrightnessIntent")
+        .one_of("BrightenKeyword", "DimKeyword")
+        .one_of("Label", "Group", "RoomKeyword"))
+    def handle_change_brightness_intent(self, message):
+        if "BrightenKeyword" in message.data:
+            brightness = .1
+        else:
+            brightness = -.1
+
+        selector = self._choose_selector(message)
+
+        # TODO Get the library updated to include this feature
+        #self.lifx.set_delta(selector=selector, brightness=brightness)
+
+        self.speak_dialog("done")
+
 def create_skill():
+    """
+    Initialize the skill
+    """
     return LifxSkill()
